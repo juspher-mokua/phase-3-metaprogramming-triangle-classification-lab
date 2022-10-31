3017dbefd1d378f28a6add41ade4e7538351ccf1
Juspher Mokua <juspermokua6@gmail.com>
2022-10-31 23:15:10 +0300

    done assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
   # write code here
+  attr_accessor :sides
+  @sides = []
+
+  def initialize(a, b, c)
+    @sides = [a, b, c]
+    # '!' is the bang method in ruby, it will replace the existing value
+    sides.sort!
+  end
+
+  def kind
+    if @sides.any?{|side| side <= 0} || ((@sides[0] + @sides[1]) <= @sides[2])
+      raise TriangleError
+    elsif @sides.uniq.length == 1
+      :equilateral
+    elsif @sides.uniq.length == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+    end
+  end
+
 end
